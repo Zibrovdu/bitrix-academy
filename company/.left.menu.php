00e331ff622bc56b368c3cf3dac30e6e7463f397
Dmitriy Zibrov <zibrovdu@gmail.com>
2024-10-28 20:58:33 +0300

    Файлы публичной части портала

---
+++ company/.left.menu.php
@@ -0,0 +1,53 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Структура компании", 
+		"/company/vis_structure.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Поиск сотрудника", 
+		"/company/index.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Кадровые изменения", 
+		"/company/events.php", 
+		Array(), 
+		Array(), 
+		"CBXFeatures::IsFeatureEnabled('StaffChanges')" 
+	),
+	Array(
+		"Доска почета", 
+		"/company/leaders.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Дни рождения", 
+		"/company/birthdays.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Фотогалерея", 
+		"/company/gallery/", 
+		Array(), 
+		Array(), 
+		"CBXFeatures::IsFeatureEnabled('Gallery')" 
+	),
+	Array(
+		"Новая страница", 
+		"/company/novaya-stranitsa.php", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
